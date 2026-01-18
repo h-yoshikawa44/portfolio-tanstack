92bbcf6d37f281ccc831e9bb3d2beb697d56ebd6
h-yoshikawa44 <43331308+h-yoshikawa44@users.noreply.github.com>
2026-01-18 19:06:43 +0900

    feat: Header, Footerのストーリーを作成(#4)

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,6 +1,6 @@
 export function Footer() {
   return (
-    <footer className="from-brand-start to-brand-end bg-linear-to-b py-3 text-center text-invert">
+    <footer className="from-brand-start to-brand-end bg-linear-to-b p-3 text-center text-invert w-full">
       © 2026 h-yoshikawa44 Built with TanStack Start.
     </footer>
   );
