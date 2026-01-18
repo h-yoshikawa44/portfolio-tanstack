5a7bd746496a3f90d8e1aa391a1031e5aeec052d
h-yoshikawa44 <43331308+h-yoshikawa44@users.noreply.github.com>
2026-01-18 18:13:40 +0900

    feat: ヘッダーとフッターを作成 + ベースレイアウトに組み込み(#4)

---
+++ src/components/Footer.tsx
@@ -0,0 +1,7 @@
+export function Footer() {
+  return (
+    <footer className="from-brand-start to-brand-end bg-linear-to-b py-3 text-center text-invert">
+      © 2026 h-yoshikawa44 Built with TanStack Start.
+    </footer>
+  );
+}
--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,8 +1,8 @@
 import { TanStackDevtools } from '@tanstack/react-devtools';
 import { createRootRoute, HeadContent, Scripts } from '@tanstack/react-router';
 import { TanStackRouterDevtoolsPanel } from '@tanstack/react-router-devtools';
-
-import Header from '@/components/Header';
+import { Footer } from '@/components/Footer';
+import { Header } from '@/components/Header';
 
 import appCss from '@/styles.css?url';
 
@@ -40,6 +40,7 @@ function RootDocument({ children }: { children: React.ReactNode }) {
       <body>
         <Header />
         {children}
+        <Footer />
         <TanStackDevtools
           config={{
             position: 'bottom-right',
