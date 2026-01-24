b5a78c5d13e5604de8132e3664aa5eec44f1a7c3
h-yoshikawa44 <43331308+h-yoshikawa44@users.noreply.github.com>
2026-01-24 18:52:30 +0900

    fix: ProductItemのストーリーのargsを定数から指定するようにした(#6)

--- src/features/product/ProductItem/ProductItem.stories.tsx
+++ src/features/product/ProductItem/ProductItem.stories.tsx
@@ -1,7 +1,5 @@
 import type { Meta, StoryObj } from '@storybook/react-vite';
-import { Image } from '@unpic/react';
-import changeOfPace from '@/assets/images/products/change-of-pace/overview.png';
-
+import { MANAGEMENTS } from '@/data/myProducts';
 import { ProductItem } from './ProductItem';
 
 const meta = {
@@ -22,27 +20,5 @@ export const Default: Story = {
       );
     },
   ],
-  args: {
-    name: 'Change Of Pace',
-    description: (
-      <>
-        Astro 製の個人ブログ。
-        <br />
-        2023/07に Jekyll から移行。テーマは使用せず自作。
-      </>
-    ),
-    ProductImg: (
-      <Image
-        className="w-full h-45 rounded-tl-xl rounded-tr-xl object-cover object-[50%_0]"
-        src={changeOfPace}
-        alt="Change Of Pace - Overview"
-        layout="constrained"
-        width={300}
-        height={180}
-        priority
-      />
-    ),
-    productLink: 'https://changeofpace.site',
-    repositoryUrl: 'https://github.com/h-yoshikawa44/change-of-pace-astro',
-  },
+  args: MANAGEMENTS[0],
 };
