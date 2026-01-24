0c5a19e36a42ce4d1a3f28f8ca33f289a459cfeb
h-yoshikawa44 <43331308+h-yoshikawa44@users.noreply.github.com>
2026-01-24 17:54:16 +0900

    feat: next/imageに相当する機能の案内があったので適用(#5)

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,4 +1,5 @@
 import { Link } from '@tanstack/react-router';
+import { Image } from '@unpic/react';
 import myIcon from '@/assets/images/my-icon.png';
 
 export function Header() {
@@ -6,7 +7,7 @@ export function Header() {
     <header className="sticky top-0 z-10 md:px-14 py-3 px-4 from-brand-start to-brand-end bg-linear-to-b flex justify-between">
       <Link to="/">
         <div className="p-1 rounded-[50%] bg-invert w-12 h-12 flex justify-center items-center">
-          <img src={myIcon} alt="myIcon" width={44} height={32} />
+          <Image src={myIcon} alt="myIcon" width={44} height={32} />
         </div>
       </Link>
       <nav>
