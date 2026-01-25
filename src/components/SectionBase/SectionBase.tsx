97c96068d1f103a99c01f81da433e18f1a736475
h-yoshikawa44 <43331308+h-yoshikawa44@users.noreply.github.com>
2026-01-25 18:13:58 +0900

    feat: プライバシーポリシーページの作成(#7)

--- src/components/SectionBase/SectionBase.tsx
+++ src/components/SectionBase/SectionBase.tsx
@@ -2,7 +2,7 @@ import type { PropsWithChildren } from 'react';
 
 export function SectionBase({ children }: PropsWithChildren) {
   return (
-    <section className="bg-brand-secondary w-full md:px-14 py-8 md:py-12 px-4 text-invert">
+    <section className="bg-brand-secondary w-full md:px-14 py-8  px-4 text-invert">
       {children}
     </section>
   );
