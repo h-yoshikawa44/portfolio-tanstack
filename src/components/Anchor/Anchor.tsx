01db6901c24ea865dc5370742d59a8334e98319a
h-yoshikawa44 <43331308+h-yoshikawa44@users.noreply.github.com>
2026-01-25 18:55:48 +0900

    feat: Anchorコンポーネントは別タブで開くようにした(#7)

--- src/components/Anchor/Anchor.tsx
+++ src/components/Anchor/Anchor.tsx
@@ -1,20 +1,23 @@
 import { clsx } from 'clsx';
 import type { ComponentProps } from 'react';
 
-export type AnchorProps = ComponentProps<'a'> & {
+export type AnchorProps = Omit<ComponentProps<'a'>, 'target' | 'rel'> & {
   disabledHoverUnderline?: boolean;
 };
 
 export function Anchor({
   disabledHoverUnderline = false,
+  className,
   children,
   ...props
 }: AnchorProps) {
   return (
     <a
-      className={clsx('text-invert', {
+      className={clsx('text-invert', className, {
         'hover:underline underline-offset-2': !disabledHoverUnderline,
       })}
+      target="_blank"
+      rel="noopener noreferrer"
       {...props}
     >
       {children}
