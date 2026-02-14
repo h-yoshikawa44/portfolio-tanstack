cf7ed503d1d6a9780f63fdac1a54e4ddc7fe6407
h-yoshikawa44 <43331308+h-yoshikawa44@users.noreply.github.com>
2026-02-14 18:52:01 +0900

    feat: SectionBaseでFadeContentを導入してみた(#11)

--- src/components/SectionBase/SectionBase.tsx
+++ src/components/SectionBase/SectionBase.tsx
@@ -1,9 +1,12 @@
+import { FadeContent } from '@/anime-components/FadeContent/FadeContent';
 import type { PropsWithChildren } from 'react';
 
 export function SectionBase({ children }: PropsWithChildren) {
   return (
-    <section className="bg-brand-secondary w-full md:px-14 py-10 px-4 text-invert">
-      {children}
-    </section>
+    <FadeContent blur threshold={0.05}>
+      <section className="bg-brand-secondary w-full md:px-14 py-10 px-4 text-invert">
+        {children}
+      </section>
+    </FadeContent>
   );
 }
