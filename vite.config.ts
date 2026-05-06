1e9869f1e50bdb18d3384a77bd7076684815b791
h-yoshikawa44 <43331308+h-yoshikawa44@users.noreply.github.com>
2026-05-06 14:03:47 +0900

    remove: Vite 8系に伴い不要になったのでvite-tsconfig-pathsを除去(#27)

--- vite.config.ts
+++ vite.config.ts
@@ -4,16 +4,11 @@ import { devtools } from '@tanstack/devtools-vite';
 import { tanstackStart } from '@tanstack/react-start/plugin/vite';
 import viteReact from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
-import viteTsConfigPaths from 'vite-tsconfig-paths';
 
 const config = defineConfig({
   plugins: [
     devtools(),
     cloudflare({ viteEnvironment: { name: 'ssr' } }),
-    // this is the plugin that enables path aliases
-    viteTsConfigPaths({
-      projects: ['./tsconfig.json'],
-    }),
     tailwindcss(),
     tanstackStart({
       prerender: {
@@ -27,6 +22,9 @@ const config = defineConfig({
     }),
     viteReact(),
   ],
+  resolve: {
+    tsconfigPaths: true
+  }
 });
 
 export default config;
